aca58fd27fed2daa458e2da8bb95867c2b4a95d1
Death <21cookej@roundwoodpark.co.uk>
2025-07-17 10:44:33 +0100

    Update site.ts

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -38,28 +38,6 @@ export const siteConfig = {
     github: 'https://github.com/21cookej/21cookej.github.io',
     githubAccount: '',
   },
-  socialLinks: [
-    {
-      title: 'Facebook',
-      href: `${env.NEXT_PUBLIC_FACEBOOK}`,
-      icon: Icons.facebook,
-    },
-    {
-      title: 'Instagram',
-      href: `${env.NEXT_PUBLIC_INSTAGRAM}`,
-      icon: Icons.instagram,
-    },
-    {
-      title: 'Twitter',
-      href: `${env.NEXT_PUBLIC_TWITTER}`,
-      icon: Icons.twitter,
-    },
-    {
-      title: 'Youtube',
-      href: `${env.NEXT_PUBLIC_YOUTUBE}`,
-      icon: Icons.youtube,
-    },
-  ],
   footerItems: [
     { title: 'Audio Description', href: '/' },
     { title: 'Help Center', href: '/' },
