4605e2c758866a56eb8b7f5322a1883955cabf68
Death <21cookej@roundwoodpark.co.uk>
2025-07-17 10:41:33 +0100

    Update site.ts

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -35,7 +35,7 @@ export const siteConfig = {
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/images/hero.jpg`,
   links: {
     twitter: `${env.NEXT_PUBLIC_TWITTER}`,
-    github: 'https://github.com/21cookej/movie-web-better/tree/dev?tab=readme-ov-file',
+    github: 'https://github.com/21cookej/21cookej.github.io',
     githubAccount: '',
   },
   socialLinks: [
